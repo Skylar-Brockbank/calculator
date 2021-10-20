cb12b03f1d74326bb08f2870138f5b4e260d43a6
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-20 12:00:01 -0700

    make userinterface and business logic operable
    Co-authored-by: Hannah Young <hannah@corgibyte.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,27 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-function farenheitToCelsius(tempF){
-  return (tempF-32)*(5/9);
-}
+// UI
+
+$(document).ready(function() {
+  $("#addButton").click(function() {
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+
+    $("#answerField").text(number1 + " + " + number2 + " = " + add(number1, number2));
+  });
+});
+
+
+
+
+// function farenheitToCelsius(tempF){
+//   return (tempF-32)*(5/9);
+// }
 
 // const number1 = parseInt(prompt("Enter a number:"));
 // const number2 = parseInt(prompt("Enter another number:"));
 // const result = divide(number1, number2);
-const temp = parseInt(prompt("Please enter the temperature in degrees Farenheit"))
-const result = farenheitToCelsius(temp);
-alert(result);
+// const temp = parseInt(prompt("Please enter the temperature in degrees Farenheit"))
+// const result = farenheitToCelsius(temp);
+// alert(result);
